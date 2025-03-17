ef329c5c4e85e423c5f8be40bad3915bfe3a3f5e
Siddhant444-dev <sidsinha491@gmail.com>
2025-03-18 00:51:11 +0530

    Initial commit

---
+++ src/main/java/com/siddhant/audio_transcribe/AudioTranscribeApplication.java
@@ -0,0 +1,13 @@
+package com.siddhant.audio_transcribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioTranscribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioTranscribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddhant/audio_transcribe/AudioTranscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhant.audio_transcribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
